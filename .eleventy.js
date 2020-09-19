451ec7987b7d770ee5b096d392e564becef7e30c
Jabran Rafique <hello@jabran.me>
2020-09-20 00:38:07 +0100

    more config, add styling

--- .eleventy.js
+++ .eleventy.js
@@ -5,10 +5,18 @@ module.exports = function (eleventyConfig) {
   // copy static assets
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPassthroughCopy({ 'public/**': '.' });
+  eleventyConfig.addPassthroughCopy({
+    'node_modules/normalize.css/normalize.css': 'assets/css/normalize.css'
+  });
 
   // add dump for debugging
   eleventyConfig.addFilter('dump', function (value) {
     return util.inspect(value, { depth: 5 });
+  });
+
+  // format numbers
+  eleventyConfig.addFilter('number_format', function (value) {
+    return Number(value).toLocaleString();
   });
 
   // add 404
