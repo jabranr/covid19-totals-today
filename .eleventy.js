bc8bc5cd3842d9baff3c8f1c3d0dc93b4b556d3f
Jabran Rafique <hello@jabran.me>
2020-09-19 01:34:06 +0100

    add 404, update styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,27 @@
+const fs = require('fs');
 const util = require('util');
 
 module.exports = function (eleventyConfig) {
+  // copy static assets
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPassthroughCopy({ 'public/**': '.' });
 
+  // add dump for debugging
   eleventyConfig.addFilter('dump', function (value) {
     return util.inspect(value, { depth: 5 });
+  });
+
+  // add 404
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, bs) {
+        bs.addMiddleware('*', (req, res) => {
+          const content404 = fs.readFileSync('_site/404/index.html');
+          res.write(content404);
+          res.writeHead(404);
+          res.end();
+        });
+      }
+    }
   });
 };
