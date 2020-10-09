2da28e791c6cc88368081a7abc37807487a930f3
Jabran Rafique <hello@jabran.me>
2020-10-10 00:51:57 +0100

    generate dynamic og images

--- generate-og-images.js
+++ generate-og-images.js
@@ -4,51 +4,63 @@ const jimp = require('jimp');
 
 const { toSlug } = require('./util');
 const continents = require('./_data/continents.json');
-const watermarkPath = path.resolve(__dirname, './assets/images/apple-touch-icon-200x200.png');
+const watermarkPath = path.resolve(__dirname, './assets/images/apple-touch-icon-512x512.png');
 
 async function capture(url, file, page) {
   try {
     console.log(`- Visiting: ${url}`);
     await page.goto(url);
-    await page.setViewport({ width: 1024, height: 576, deviceScaleFactor: 2 });
+    await page.setViewport({ width: 1200, height: 630, deviceScaleFactor: 2 });
     await page.screenshot({ path: file });
     console.log(`- Captured: ${file}`);
 
-    // const [img, watermark] = await Promise.all([jimp.read(file), jimp.read(watermarkPath)]);
+    const [img, watermark] = await Promise.all([jimp.read(file), jimp.read(watermarkPath)]);
+    const composedImage = await img.composite(watermark, img.bitmap.width - (watermark.bitmap.width + 75), 75, [
+      {
+        mode: jimp.BLEND_SCREEN,
+        opacitySource: 0.1,
+        opacityDest: 1
+      }
+    ]);
 
-    // const composedImage = await img.composite(watermark, img.bitmap.width - (watermark.bitmap.width + 10), 20, [
-    //   {
-    //     mode: jimp.BLEND_SCREEN,
-    //     opacitySource: 0.1,
-    //     opacityDest: 1
-    //   }
-    // ]);
-
-    // await composedImage.writeAsync(file);
-    // console.log(`- Watermarked: ${file}`);
+    await composedImage.writeAsync(file);
+    console.log(`- Watermarked: ${file}`);
+    console.log(`======================`);
   } catch (err) {
     console.log(err);
-  } finally {
     process.exit(0);
   }
 }
 
 (async () => {
+  const startTime = +new Date();
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
+
+  await capture(
+    `file://${path.resolve(__dirname, `_site/index.html`)}`,
+    path.resolve(__dirname, `./assets/images/opengraph/homepage.png`),
+    page
+  );
 
   for (const c of continents) {
     await capture(
       `file://${path.resolve(__dirname, `_site/continents/${toSlug(c.continent)}/index.html`)}`,
-      path.resolve(__dirname, `./assets/images/opengraph/continent-${toSlug(c.continent)}.png`),
+      path.resolve(__dirname, `./assets/images/opengraph/continents-${toSlug(c.continent)}.png`),
       page
     );
 
-    // c.countries.forEach((cc) => {
-    //   console.log(toSlug(cc));
-    // });
+    for (const country of c.countries) {
+      await capture(
+        `file://${path.resolve(__dirname, `_site/countries/${toSlug(country)}/index.html`)}`,
+        path.resolve(__dirname, `./assets/images/opengraph/countries-${toSlug(country)}.png`),
+        page
+      );
+    }
   }
 
-  await browser.close();
-  console.log('- Done');
+  browser.close();
+  const totalTime = new Date(+new Date() - startTime).getSeconds();
+  const timeTaken = totalTime / 60 >= 1 ? `${totalTime / 60}m` : `${totalTime}s`;
+  console.log(`- Completed in ${timeTaken}`);
 })();
